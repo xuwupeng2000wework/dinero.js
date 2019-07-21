189842f773bdf5b2ac882a23743950c88199256c
Sarah Dayan <dayan.sarah@gmail.com>
2019-07-21 23:42:58 +0200

    chore: move index and mark package as side-effect free

--- packages/utilities/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as countFractionDigits } from './src/countFractionDigits'
-export { default as isEven } from './src/isEven'
-export { default as isFloat } from './src/isFloat'
-export { default as isHalf } from './src/isHalf'
---
+++ packages/utilities/src/index.ts
@@ -0,0 +1,4 @@
+export { default as countFractionDigits } from './countFractionDigits'
+export { default as isEven } from './isEven'
+export { default as isFloat } from './isFloat'
+export { default as isHalf } from './isHalf'
